3a19a4cac0447f56e2c1d912c4a46a94f23ba750
GiacomoMaletto <malettogiacomo6@gmail.com>
2022-01-27 02:57:45 +0100

    grafico liscio

--- main.cpp
+++ main.cpp
@@ -3,17 +3,21 @@
 #include <math.h>
 
 int main(){
-  FILE *output = fopen("output.txt", "w");
+  FILE *order = fopen("order.txt", "w");
+  FILE *points = fopen("points.txt", "w");
   
-  int N = 300;
+  int N = 100;
+  for(int i = 0; i < N-1; i++){
+    for(int j = 0; j < N-1; j++){
+      fprintf(order, "%d\n%d\n%d\n%d\n%d\n%d\n", N*i+j+1, N*(i+1)+j+1, N*i+j+2, N*i+j+2, N*(i+1)+j+1, N*(i+1)+j+2);
+    }
+  }
   for(int i = 0; i < N; i++){
     for(int j = 0; j < N; j++){
       float x = ((float)i)/N;
       float y = ((float)j)/N;
       float z = .2 * (sin(5.0*x) + sin(5.0*y));
-      fprintf(output, "%f %f %f\n", x, y, z);
+      fprintf(points, "%f %f %f\n", x, y, z);
     }
   }
-
-  fclose(output);
 }
--- main.lua
+++ main.lua
@@ -11,13 +11,15 @@ local V, M = unpack(require "vector")
 love.mouse.setRelativeMode(true)
 love.graphics.setDefaultFilter("nearest", "nearest")
 love.graphics.setPointSize(1)
+love.graphics.setDepthMode("less", true)
+love.graphics.setBackgroundColor(.1, .1, .1)
 
 local sw, sh = love.graphics.getDimensions()
 
 local camera = {}
-camera.xyz = {0.5, 0.5, 1}
-camera.phi = 0
-camera.theta = math.rad(179)
+camera.xyz = {3/2, 1/2, 2}
+camera.phi = math.rad(180)
+camera.theta = math.rad(150)
 camera.speed = 1
 function camera.dir()
   return V.spherical(1, camera.theta, camera.phi)
@@ -37,16 +39,47 @@ end
 
 local points = {}
 do
-  local input = love.filesystem.newFile("output.txt")
+  local input = love.filesystem.newFile("points.txt")
   for line in input:lines() do
     local x, y, z = string.match(line, "(.+) (.+) (.+)")
     x, y, z = tonumber(x), tonumber(y), tonumber(z)
-    points[#points+1] = {x, y, z}
+    local r = .6*(.5+2*z)
+    local g = .6*(.5+1.1*z)
+    local b = (.5+1.1*z)
+    points[#points+1] = {x, y, z, r, g, b, 1}
   end
   input:close()
 end
-local mesh = love.graphics.newMesh({{"VertexPosition", "float", 3}}, points, "points", "static")
+local mesh = love.graphics.newMesh({{"VertexPosition", "float", 3}, {"VertexColor", "float", 4}}, points, "triangles", "static")
+do
+  local order = {}
+  local input = love.filesystem.newFile("order.txt")
+  for line in input:lines() do
+    n = string.match(line, "(.+)")
+    order[#order+1] = n
+  end
+  mesh:setVertexMap(order)
+end
 
+local eps = .003
+local x_arrow = love.graphics.newMesh({{"VertexPosition", "float", 3}},
+  {{0, -eps, -eps}, {0, -eps,  eps}, {0, eps, eps},
+   {0, -eps, -eps}, {0,  eps, -eps}, {0, eps, eps},
+   {1, -eps, -eps}, {1, -eps,  eps}, {1, eps, eps},
+   {1, -eps, -eps}, {1,  eps, -eps}, {1, eps, eps},
+   
+   {0, -eps, -eps}, {1, -eps, -eps}, {0, -eps, eps},
+   {0, -eps,  eps}, {1, -eps, -eps}, {1, -eps, eps},
+   
+   {0, -eps, -eps}, {1, -eps, -eps}, {0, eps, -eps},
+   {0,  eps, -eps}, {1, -eps, -eps}, {1, eps, -eps},
+   
+   {0, -eps, eps}, {1, -eps, eps}, {0, eps, eps},
+   {0,  eps, eps}, {1, -eps, eps}, {1, eps, eps},
+
+   {0, eps, -eps}, {1, eps, -eps}, {0, eps, eps},
+   {0, eps,  eps}, {1, eps, -eps}, {1, eps, eps}},
+  "triangles", "static")
 
 function love.mousemoved(x, y, dx, dy, istouch)
   camera.phi = camera.phi - dx/200
@@ -88,5 +121,6 @@ function love.draw()
   love.graphics.setShader(pShader)
   pShader:send("MVP", camera.mvp())
   love.graphics.draw(mesh)
+  love.graphics.draw(x_arrow)
   love.graphics.setShader()
 end
